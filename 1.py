b5d31fc663502ec313815f4c3dabdf12fdaa369f
SDURJY2019 <85665045+SDURJY2019@users.noreply.github.com>
2022-10-09 19:11:10 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,7 +8,7 @@ num1 = 0
 def gettoken(refresh_token):
     headers={'Content-Type':'application/x-www-form-urlencoded'
             }
-    data={'grant_type': 'refresh_token',
+   data={'grant_type': 'refresh_token',
           'refresh_token': refresh_token,
           'client_id':id,
           'client_secret':secret,
