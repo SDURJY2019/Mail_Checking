1e2989c33909d6a4a63cdbe754af99a305c8be31
SDURJY2019 <85665045+SDURJY2019@users.noreply.github.com>
2022-10-09 19:22:38 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,7 +7,7 @@ num1 = 0
 
 def gettoken(refresh_token):
     headers={'Content-Type':'application/x-www-form-urlencoded'}
-    data={'grant_type':'refresh_token','refresh_token':refresh_token,'client_id':id,'client_secret':secret,'redirect_uri':'http://localhost:53682/'}
+    data={'grant_type':'refresh_token','refresh_token':refresh_token,'client_id':id,'client_secret':secret,'redirect_uri':'http://localhost:53682/'}    
     html=req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt=json.loads(html.text)
     refresh_token=jsontxt['refresh_token']
